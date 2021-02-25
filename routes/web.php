abab829c58d397991c410bde4272df263a6c5f63
Akatsuki Tanaka <shibaluke5526@gmail.com>
2021-02-25 12:03:41 +0900

    ログインフォーム表示

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,7 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'UserController@login')->name('user.login');
+/*
+Route::post('/user/login', 'UserController@login')->name('user.login');
+*/
